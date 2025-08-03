e05bbfaae7e915bf3514e31b8fc206865f73e1c8
rifatbhuiya <rifatbhuiya.personal@gmail.com>
2025-08-03 20:52:47 +0600

    updated header

--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
 (function () {
     "use-strict";
-    
+
+    // Window on Scroll
+    $(window).on('scroll', function () {
+        let scrolling = $(this).scrollTop();
+        
+        if (scrolling > 50) {
+            $('.main-header').addClass('fixed-header');
+        } else {
+            $('.main-header').removeClass('fixed-header');
+        }
+    });
 })();
